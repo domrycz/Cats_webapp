82ba482b2df253815e9457762a2df8719a40f839
Kontor <domrycz@gmail.com>
2017-04-23 19:55:54 +0200

    regular expressions added

--- src/main/java/pl/domrycz/Interface.java
+++ src/main/java/pl/domrycz/Interface.java
@@ -3,18 +3,21 @@ package pl.domrycz;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import pl.domrycz.model.Cat;
 
 /**
- * TERAZ ZADANIE 4 - WYRAŻENIA REGULARNE. DATY NIE ZMIENIAĆ ALE RESZTĘ OGARNĄĆ!
+ * TERAZ ROZDZIAŁ 5 - KOLEKCJE
  */
 
 
 public class Interface {
-    static Scanner input = new Scanner(System.in);
+    private static Scanner input = new Scanner(System.in);
 
-    static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-    static String getUserInput() {
+    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    private static String getUserInput() {
         return input.nextLine();
     }
     public static void main(String[] args) {
@@ -23,24 +26,36 @@ public class Interface {
         cat.setKeeper(getUserInput());
         System.out.print("Enter cat's name: ");
         cat.setName(getUserInput());
+
+        String inputLoaded;
+        Boolean isRight;
+
+        Pattern datePattern = Pattern.compile("[1-2][0-9]{3}-[0-1][0-9]-[0-3][0-9]");
         do {
             System.out.print("Enter cat's birth date (yyyy-mm-dd): ");
-            try {
-                cat.setDate(sdf.parse(getUserInput()));
-            } catch (ParseException pe) {
+            inputLoaded = getUserInput();
+            Matcher matcher = datePattern.matcher(inputLoaded);
+            isRight = matcher.matches();
+            if(isRight) {
+                try {
+                    cat.setDate(sdf.parse(inputLoaded));
+                } catch (ParseException pe) { }
+            } else
                 System.out.println("Wrong date format!");
-            }
-        } while (cat.getDate() == null);
+        } while (cat.getDate() == null || !isRight);
+
+        Pattern weightPattern = Pattern.compile("[0-9]{1,2}(\\.[0-9])?");
         do {
-            try {
-                System.out.print("Enter cat's weight: ");
-                cat.setWeight(Float.parseFloat(getUserInput()));
-            } catch (NullPointerException npe) {
-                System.out.println("You have to enter cat's weight!");
-            } catch (NumberFormatException nfe) {
-                System.out.println("Enter weight in format digit.digit!");
-            }
-        } while (cat.getWeight() == null);
+            System.out.print("Enter cat's weight (digit.digit): ");
+            inputLoaded = getUserInput();
+            Matcher matcher = weightPattern.matcher(inputLoaded);
+            isRight = matcher.matches();
+            if(isRight)
+                cat.setWeight(Float.parseFloat(inputLoaded));
+        } while(cat.getWeight() == null || !isRight);
+
+        System.out.println("Cat's data saved!");
     }
+
 
 }
